f90507f5793db9d29938b34e4c2d7d3b3cccd41c
jiheeyu <j_yu@fanshaweonline.ca>
2020-11-21 11:51:04 +0900

    fetchAndsome

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-const myVM = (() => {
+import { fetchData } from "./components/DataMiner.js";
+
+(() => {
     let  vue_vm = new Vue({
         // link vue to an element in our HTML
         // el: "#app",
@@ -9,18 +11,24 @@ const myVM = (() => {
             removeAformat: true,
             showBioData: true,
 
-            profs : [
-                {name: "Justin", role:"coordinator", nickname:"nitsuJ"},
-                {name: "John", role:"prof", nickname:"super chill"},
-                {name: "Joe", role:"prof", nickname:"nitsuJ"}
-            ]
+            profs : [],
+            currentProfData: {}
+
+            // profs : [
+            //     {name: "Justin", role:"coordinator", nickname:"nitsuJ"},
+            //     {name: "John", role:"prof", nickname:"super chill"},
+            //     {name: "Joe", role:"prof", nickname:"nitsuJ"}
+            // ]
         },
 
         //this is "mounted" lifecycle hook. vue is done creating itself, and has attacted itself to the 'app'div on the page.
         mounted: function() {
-            console.log("Vue is mounted!");
+            console.log("Vue is mounted, trying to fetch call");
 
-            this.profs.push({name: "Jarrod", role: "supermodel prof", nickname: "Zoolander"});
+            fetchData("./includes/index.php").then(data => this.profs = data).catch(err => console.log(err));
+            // may be you can try .then(data => {
+            // data.forEach(prof => this.profs.push(prof))
+            //})
             
         },
 
@@ -38,13 +46,23 @@ const myVM = (() => {
                 console.log("header")
             },
 
-            removeProf(target) {
+            showProf(target) {
                 // remove this prof from professors array
-                console.log('click to remove prof', target, target.name);
+                console.log('click to view prof bio data', target, target.name);
                 // the "this" keyword inside a vue instance REFERS to the vue instance itself by default
 
                 // toggle the property between true an false using a ternary statement
-                this.showBioData = this.showBioData ? false : true
+                this.showBioData = this.showBioData ? false : true;
+
+                // make the selected profs data visible
+                this.currentProfData = target;
+            },
+
+            removeProf(target) {
+                console.log('click to view prof bio data', target, target.name);
+
+                //this.profs.splice(this.profs.indexOf(target),1);
+                this.$delete(this.profs, target);
             }
         }
 
